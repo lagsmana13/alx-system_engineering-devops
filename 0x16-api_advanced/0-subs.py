0a7b4b109cd28ae4f6838948ba26137457ed51f2
lagsmana13 <lagsmana@yahoo.com>
2024-05-21 02:46:00 -0700

    18X

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
 """
-Piece of code that retrieves the total number of users on a given Reddit subreddit.
+Function to fetch the number of subscribers in a specific Reddit community.
 """
 
 import requests
 
-def get_subscriber_count(community):
-"""Obtain the total number of users on a given subreddit."""
-url = "https://www.reddit.com/r/{}/about.json".format(community)
-headers = {"User-Agent": "Mozilla/5.0"}
-response = requests.get(url, headers=headers, allow_redirects=False)
-if response.status_code == 200:
-data = response.json()
-subscriber_count = data['data']['subscribers']
-return subscriber_count
-else:
-return 0
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    If the subreddit is invalid, returns 0.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers, or 0 if the subreddit is invalid.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+
+    data = response.json().get("data", {})
+    return data.get("subscribers", 0)
